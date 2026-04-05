c0d03b04d35f48c083522ac2913bb4e76fa4c5c4
Filip <fmojkovic@google.com>
2026-04-05 17:09:19 +0200

    Dodat Validator

---
+++ InstrumentShop/InstrumentValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using InstrumentShop.Shared;
+
+public class InstrumentValidator : AbstractValidator<InstrumentDto>
+{
+    public InstrumentValidator()
+    {
+        RuleFor(x => x.Name).NotEmpty().Length(3, 50);
+        RuleFor(x => x.Price).GreaterThan(0).WithMessage("Cena mora biti veća od nule.");
+    }
+}
